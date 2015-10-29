45f23de0b08fe1c77db26d1cc12de1963ec0af89
CalebTVanDyke <ctvandyke24@gmail.com>
2015-10-29 16:30:42 -0500

    Added bootstrap files and HTML template

--- public_html/index.php
+++ public_html/index.php
@@ -1,11 +1,29 @@
-CTYPE html PUBLIC "-//IETF//DTD HTML 2.0//EN">
-<HTML>
-   <HEAD>
-      <TITLE>
-         Renegades 
-      </TITLE>
-   </HEAD>
-<BODY>
-   <h1>Hello, world!</h1> 
-</BODY>
-</HTML>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Renegades</title>
+<link rel="stylesheet" type="text/css" media="all"
+	href="css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" media="all"
+	href="css/main.css">
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/smoothness/jquery-ui.css" />
+<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
+
+</head>
+<body>
+	<div class="container">
+		<div class="header">
+			<h1>Game Renegades</h1>
+		</div>
+		<ul class="nav nav-tabs nav-justified">
+			<li class="active"><a id="home">Home</a></li>
+			<li><a id="home">Games</a></li>
+			<li><a id="home">Tournaments</a></li>
+			<li><a id="home">Leaderboards</a></li>
+			<li><a id="home">Profile</a></li>
+		</ul>
+		<div id="content"></div>
+	</div>
+</body>
+</html>
