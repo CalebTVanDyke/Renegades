9950b667c3ac4999ee3339ce24fd317a6ba2a36e
Ian Pierce <iandpierce@gmail.com>
2015-12-01 11:12:04 -0600

    Created profile page

---
+++ public_html/profile.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Renegades</title>
+    <link rel="stylesheet" type="text/css" media="all"
+          href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" media="all"
+          href="css/main.css">
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/smoothness/jquery-ui.css" />
+    <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
+</head>
+<body>
+<div class="container">
+    <div class="header">
+        <h1>Game Renegades</h1>
+    </div>
+    <ul class="nav nav-tabs nav-justified">
+        <li><a id="home">Home</a></li>
+        <li><a id="home">Games</a></li>
+        <li><a id="home">Tournaments</a></li>
+        <li><a id="home">Leaderboards</a></li>
+        <li class="active"><a id="home">Profile</a></li>
+    </ul>
+    <div id="content">
+        <div class="row">
+            <div class="col-sm-12 col-md-4">
+                <div class="centerBlock">
+                    <img src="http://ui.uniteddogs.com/img/ui/user_icons/_no_avatar_f_180x180.png" class="img-thumbnail">
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-8">Username and Bio and Position</div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12 col-md-4">
+                <div class="short-div">Contact Info</div>
+                <div class="short-div">Games Owned</div>
+            </div>
+            <div class="col-sm-12 col-md-8">Recent Activity Log</div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
