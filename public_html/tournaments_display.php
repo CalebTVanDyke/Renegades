5260dfa3347e638025c811d1f910dd62910b8c52
aojeleye <aojeleye@iastate.edu>
2015-12-01 20:30:00 -0600

    Small changes to tournament display file.

--- public_html/tournaments_display.php
+++ public_html/tournaments_display.php
@@ -59,7 +59,7 @@ $(function() {
  </script>-->
  
 	<div id="save"></div>
-	<div id="dataOutput"></div>
+	<div id="doubleElimination"></div>
 	<script>
 	
 var saveData = {
@@ -98,6 +98,30 @@ var bigData = {
     [[2,1]]
   ]]
 }
+
+
+var doubleEliminationData = {
+    teams : [
+      ["Team 1", "Team 2"],
+      ["Team 3", "Team 4"]
+    ],
+    results : [[      /* WINNER BRACKET */
+      [[1,2], [3,4]], /* first and second matches of the first round */
+      [[5,6]]         /* second round */
+    ], [              /* LOSER BRACKET */
+      [[7,8]],        /* first round */
+      [[9,10]]        /* second round */
+    ], [              /* FINALS */
+      [[1,12], [13,14]],
+      [[15,16]]       /* LB winner won first round so need a rematch */
+    ]]
+  }
+ 
+ /* Double elimination */
+$(function() {
+    $('#doubleElimination').bracket({
+      init: doubleEliminationData})
+  })
  
 /* Called whenever bracket is modified
  *
