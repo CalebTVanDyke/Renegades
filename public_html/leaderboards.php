377ed14741952a33941c2e3d0773345e329bb5f7
smthmelv <smthmelv@iastate.edu>
2015-12-08 22:53:45 -0600

    updated leaderboards

--- public_html/leaderboards.php
+++ public_html/leaderboards.php
@@ -183,9 +183,9 @@ error_reporting(E_ALL);
 						//var_dump($row2);
 						var_dump($mData);
 						if ($_SESSION["player_tag"] == $tData[$i]["m.player_tag"]){
-							echo ('<tr bgcolor="#FFCC99"><td><a href="profile.php?user=' .$mData[$i]['m.member_id']. '">' .($tData[$i]["m.player_tag"]). '</a></td><td>' .($wData[$i]["mg.wins"]). '</td><td>' .($lData[$i]["mg.losses"]). '</td><td>' .($pData[$i]["p.name"]). '</td></tr>');
+							echo ('<tr bgcolor="#FFCC99"><td><a href="profile.php?user=' .$mData[$i]["m.member_id"]. '">' .($tData[$i]["m.player_tag"]). '</a></td><td>' .($wData[$i]["mg.wins"]). '</td><td>' .($lData[$i]["mg.losses"]). '</td><td>' .($pData[$i]["p.name"]). '</td></tr>');
 						}else{
-							echo ('<tr><td><a href="profile.php?user=' .$mData[$i]['m.member_id']. '">' .($tData[$i]["m.player_tag"]). '</a></td><td>' .($wData[$i]["mg.wins"]). '</td><td>' .($lData[$i]["mg.losses"]). '</td><td>' .($pData[$i]["p.name"]). '</td></tr>');
+							echo ('<tr><td><a href="profile.php?user=' .$mData[$i]["m.member_id"]. '">' .($tData[$i]["m.player_tag"]). '</a></td><td>' .($wData[$i]["mg.wins"]). '</td><td>' .($lData[$i]["mg.losses"]). '</td><td>' .($pData[$i]["p.name"]). '</td></tr>');
 						}
 					}else{
 						echo ('<tr><td>' .($tData[$i]["m.player_tag"]). '</td><td>' .($wData[$i]["mg.wins"]). '</td><td>' .($lData[$i]["mg.losses"]). '</td><td>' .($pData[$i]["p.name"]). '</td></tr>');
