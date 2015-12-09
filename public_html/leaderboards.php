8974cab3ff83fac091777addace279963fb37e8a
smthmelv <smthmelv@iastate.edu>
2015-12-08 22:19:02 -0600

    updated leaderboards

--- public_html/leaderboards.php
+++ public_html/leaderboards.php
@@ -182,7 +182,7 @@ error_reporting(E_ALL);
 						}
 						//var_dump($row2);
 						var_dump($mData);
-						echo ('<tr><td><a href="profile.php?user=' . $mData[$i]["m.member_id"] . '">' .($tData[$i]["m.player_tag"]). '</a></td><td>' .($wData[$i]["mg.wins"]). '</td><td>' .($lData[$i]["mg.losses"]). '</td><td>' .($pData[$i]["p.name"]). '</td></tr>');
+						echo ('<tr><td><a href="profile.php?user="' .$mData[$i]["m.member_id"]. '"">' .($tData[$i]["m.player_tag"]). '</a></td><td>' .($wData[$i]["mg.wins"]). '</td><td>' .($lData[$i]["mg.losses"]). '</td><td>' .($pData[$i]["p.name"]). '</td></tr>');
 					}else{
 						echo ('<tr><td>' .($tData[$i]["m.player_tag"]). '</td><td>' .($wData[$i]["mg.wins"]). '</td><td>' .($lData[$i]["mg.losses"]). '</td><td>' .($pData[$i]["p.name"]). '</td></tr>');
 					}
