1c555945f0f99ad08dec605c49f1ff125c0bdb5e
smthmelv <smthmelv@iastate.edu>
2015-12-08 23:27:41 -0600

    updated leaderboards

--- public_html/leaderboards.php
+++ public_html/leaderboards.php
@@ -109,10 +109,10 @@ error_reporting(E_ALL);
 				
 			echo ('<h3>'.$selected_val.' Leaderboards</h3>');
 			?>
-			<form action="leaderboards.php" method="get">
+			<form action="#" method="get">
 					<input type="text" name="Name">
 					<!--<img src="../resources/game_images/Magnify Glass.jpg" alt="Submit" style="width:40px;height:30px;"/>-->
-					<input type="submit" name="submit2" value="Search">
+					<input type="submit" name="submit" value="Search">
 					</input>
 			</form>
 		</span>
@@ -151,11 +151,11 @@ error_reporting(E_ALL);
 				elseif (isset($_GET['sort']) && $_GET['sort'] == 'losses')
 				{
 					$result2 = $sql->runQuery("SELECT m.player_tag, mg.wins, mg.losses, p.name FROM db461rene.Game g, db461rene.Member m, db461rene.MemberGame mg, db461rene.Platform p, db461rene.GamePlatform gp WHERE (g.name='".$selected_val."') AND (m.member_id=mg.member_id) AND (g.game_id=mg.game_id) AND (g.game_id=gp.game_id) AND (gp.platform_id=p.platform_id) ORDER BY mg.losses;");
-				}elseif (isset($_GET['sort']) && isset($_GET['submit2']))
+				}elseif (isset($_GET['sort']) && isset($_GET['submit']))
 				{
 					$search = $_GET['Name'];
 					
-					$result2 = $sql->runQuery("SELECT m.player_tag, mg.wins, mg.losses, p.name FROM db461rene.Game g, db461rene.Member m, db461rene.MemberGame mg, db461rene.Platform p, db461rene.GamePlatform gp WHERE (g.name='".$selected_val."') AND (m.member_id=mg.member_id) AND (g.game_id=mg.game_id) AND (g.game_id=gp.game_id) AND (gp.platform_id=p.platform_id) AND (m.player_tag='".$search."') ORDER BY m.player_tag;");
+					$result2 = $sql->runQuery("SELECT m.player_tag, mg.wins, mg.losses, p.name FROM db461rene.Game g, db461rene.Member m, db461rene.MemberGame mg, db461rene.Platform p, db461rene.GamePlatform gp WHERE (g.name='".$selected_val."') AND (m.member_id=mg.member_id) AND (g.game_id=mg.game_id) AND (g.game_id=gp.game_id) AND (gp.platform_id=p.platform_id) AND (m.player_tag='".$search."');");
 				}else{
 					$result2 = $sql->runQuery("SELECT m.player_tag, mg.wins, mg.losses, p.name FROM db461rene.Game g, db461rene.Member m, db461rene.MemberGame mg, db461rene.Platform p, db461rene.GamePlatform gp WHERE (g.name='".$selected_val."') AND (m.member_id=mg.member_id) AND (g.game_id=mg.game_id) AND (g.game_id=gp.game_id) AND (gp.platform_id=p.platform_id) ORDER BY p.name ASC;");
 				}
