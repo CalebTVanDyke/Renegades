daf5ee0d6fc79a5caa8311bcb12694d0e3e25c96
smthmelv <smthmelv@iastate.edu>
2015-12-08 21:59:16 -0600

    updated leaderboards

--- public_html/leaderboards.php
+++ public_html/leaderboards.php
@@ -173,7 +173,7 @@ error_reporting(E_ALL);
 				for ($i = 0; $i < $count2; $i++) {
 					if (isset($_SESSION["player_tag"]) && isset($_SESSION["id"])) {
 						
-						$results = $sql->runQuery("SELECT m.member_id FROM db461rene.Member m WHERE m.player_tag !=  " .$_SESSION["player_tag"])." AND m.player_tag = " .($tData[$i]["m.player_tag"]).";");
+						$results = $sql->runQuery("SELECT m.member_id FROM db461rene.Member m WHERE m.player_tag !=  " .$_SESSION["player_tag"]." AND m.player_tag = " .($tData[$i]["m.player_tag"]).";");
 						
 						$mData = array();
 						while ($row2 = $results->fetch_row()) {
